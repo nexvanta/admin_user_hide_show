1afd5c43a979fcaa6698ba1c3490f09b21e4e27d
Md. Jamilur Rahman <eduhibe@gmail.com>
2026-03-01 17:03:17 +0600

    Add hide/show admin user feature

---
+++ __manifest__.py
@@ -0,0 +1,39 @@
+{
+    'name': 'Admin user hide and Show',
+    'version': '19.0.1.0.0',
+     'summary': '',
+    'description': """
+         
+    """,
+    'author': 'NexvantaTech',
+    'website': 'https://nexvantatech.com',
+    'category': 'Tools',
+    'depends': ['base'], 
+    'data': [
+       'views/res_users_view.xml',
+       'security/ir_rule.xml',
+    ],
+    'uninstall_hook': 'uninstall_hook',
+    
+   'images': ['static/description/Banner.png'],
+    'images': ['static/description/1.png'],
+    'images': ['static/description/2.png'],
+    'images': ['static/description/3.png'],
+    'images': ['static/description/4.png'],
+    'images': ['static/description/5.png'],
+    'images': ['static/description/6.png'],
+    'images': ['static/description/7.png'],
+    'images': ['static/description/8.png'],
+    'images': ['static/description/9.png'],
+    'images': ['static/description/10.png'],
+    'images': ['static/description/icon.png'],
+     "documentation": ["static/description/index.html"],
+    'license': 'OPL-1',
+    "live_test_url": "https://www.youtube.com/watch?v=Lk7ix8I2yh0",
+    'price': 2.00,
+    'currency': 'USD',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+
+}
