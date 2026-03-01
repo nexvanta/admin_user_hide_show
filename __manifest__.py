a12075746e174ab6a3a9252c2daa5d1d04ca5387
Md. Jamilur Rahman <eduhibe@gmail.com>
2026-03-01 22:54:36 +0600

    static

--- __manifest__.py
+++ __manifest__.py
@@ -1,9 +1,11 @@
 {
     'name': 'Admin user hide and Show',
     'version': '19.0.1.0.0',
-     'summary': '',
+     'summary': 'Admin User Hide & Show is a powerful app designed to give superadmins and managers control over visibility of admin users in your system. With this app, you can easily hide or show selected admin accounts in various modules, enhancing security, privacy, and workflow clarity. Ideal for companies with multiple admins, it ensures sensitive user accounts are only visible when needed.',
     'description': """
-         
+         Many organizations face situations where not all admin accounts should be visible to regular users or even other admins. Admin User Hide & Show addresses this by allowing you to toggle the visibility of admin users across Odoo modules like Sales, Purchase, Inventory, and more.
+
+        Whether you want to hide inactive admins, temporary users, or sensitive accounts, this app gives you a simple, intuitive interface to manage who can see what. All changes reflect immediately in the system and reports, ensuring seamless operations without compromising security.
     """,
     'author': 'NexvantaTech',
     'website': 'https://nexvantatech.com',
@@ -23,13 +25,11 @@
     'images': ['static/description/5.png'],
     'images': ['static/description/6.png'],
     'images': ['static/description/7.png'],
-    'images': ['static/description/8.png'],
-    'images': ['static/description/9.png'],
-    'images': ['static/description/10.png'],
     'images': ['static/description/icon.png'],
+    'images': ['static/description/Email.png'],
      "documentation": ["static/description/index.html"],
     'license': 'OPL-1',
-    "live_test_url": "https://www.youtube.com/watch?v=Lk7ix8I2yh0",
+    "live_test_url": "",
     'price': 2.00,
     'currency': 'USD',
     'installable': True,
